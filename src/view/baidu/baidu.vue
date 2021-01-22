eedf498647e66fba0dea3e2402df10603e95cfea
caoshuiming <2639067440@qq.com>
2021-01-22 17:07:18 +0800

    new project

---
+++ src/view/baidu/baidu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <a href="www.baidu.com">百度一下</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "baidu"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/personal/personalDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{ id }}的个人信息</h3>
+    <p>姓名：曹水明</p>
+    <p>年龄：23</p>
+    <p>籍贯：湖南</p>
+    <p>个人简介：这个人很懒，什么都没有留下哦。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "personalDetails",
+  props: ["id"]
+}
+</script>
+
+<style scoped>
+
+</style>
